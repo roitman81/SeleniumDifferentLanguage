25dcb17701bc99af8a837252385a69508b2f3a55
roitman81 <44832246+roitman81@users.noreply.github.com>
2022-08-27 15:29:18 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,28 +1,51 @@
 import pytest
+import importlib.util
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
+isedge = importlib.util.find_spec('msedge') is not None
+if isedge:
+    from msedge.selenium_tools import EdgeOptions, Edge
 
 
 def pytest_addoption(parser):
-    """Опции командной строки.
-    В командную строку передается параметр вида '--language="es"'
-    По умолчанию передается параметр, включающий английский интерфейс в браузере
-    """
-    parser.addoption('--language', action='store', default='en', help='Choose language')
+    parser.addoption(
+        '--browser_name',
+        action='store',
+        default='Chrome',  # или default=None,
+        help='Choose browser: Chrome, Firefox, Edge'
+    )
+    parser.addoption(
+        '--language',
+        action='store',
+        default='ru, en',  # или default=None,
+        help='Language'
+    )
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser(request):
-    # В переменную user_language передается параметр из командной строки
-    user_language = request.config.getoption('language')
-    
-    # Инициализируются опции браузера
-    options = Options()
-    
-    # В опции вебдрайвера передаем параметр из командной строки
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    
-    browser.implicitly_wait(5)
-    yield browser
-    browser.quit()
+    browser_name = request.config.getoption('browser_name').capitalize()
+    language = request.config.getoption('language')
+    print(f'\nStart browser {browser_name} for test..')
+    if browser_name == 'Firefox':
+        options = webdriver.FirefoxProfile()
+        options.set_preference("intl.accept_languages", language)
+        driver = webdriver.Firefox(firefox_profile=options)
+    elif isedge and browser_name == 'Edge':
+        options = EdgeOptions()
+        options.use_chromium = True
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        driver = Edge(options=options)
+    else:
+        if browser_name != 'Chrome':
+            print(f'\nDriver {browser_name} not found!')
+            print('\nStart default browser Chrome for test..')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        driver = webdriver.Chrome(options=options)
+    # driver.implicitly_wait(5)
+
+    yield driver
+
+    print('\nQuit browser..')
+    driver.quit()
