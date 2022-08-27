fcc07394f82c3878ab64c572c3bf6cd34b12bbbf
roitman81 <44832246+roitman81@users.noreply.github.com>
2022-08-27 15:28:40 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,20 +1,20 @@
 import time
 from selenium.webdriver.common.by import By
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+import pytest
+import time
+
+links = [
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/',
+    'http://selenium1py.pythonanywhere.com/catalogue/hackers-painters_185/'
+]
 
 
-class TestProductPage:
-
-    def test_button_add_to_basket_is_visible(self, browser):
-        """Тест проверяет, что страница товара
-         содержит кнопку добавления в корзину
-        """
-        # Открываем страницу товара
-        browser.get(link)
-
-        # Установлено время принудительной задержки браузера
-        # после открытия страницы, для визуальной проверки языка открытой страницы
-        time.sleep(5)
-
-        # Проверяем наличие кнопки добавления товара в корзину
-        assert browser.find_element(By.CSS_SELECTOR,"button.btn-add-to-basket")
+@pytest.mark.parametrize('link', links)
+def test_find_add_to_basket_button(browser, link):
+    browser.get(link)
+    curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    if 'fr' in curr_language:
+        time.sleep(30)
+    find_buttons = browser.find_elements(By.CSS_SELECTOR,'#add_to_basket_form .btn-add-to-basket')
+    assert len(find_buttons) > 0, 'Не найдена кнопка добавления в корзину'
+    assert len(find_buttons) < 2, 'Кнопка добавления в корзину не уникальна'
